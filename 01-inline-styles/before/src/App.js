fe5ded364572ccf2a2f5f66db7aca7dca00026bf
Kornkritpawit <konsoo@hotmail.com>
2021-02-01 22:18:07 +0700

    finish 04-1

--- 01-inline-styles/before/src/App.js
+++ 01-inline-styles/before/src/App.js
@@ -2,11 +2,29 @@ import React from 'react'
 
 import './App.css'
 import Newsletter from './Newsletter.js'
+import { ThemeProvider } from './ThemeContext.js'
+
+const theme = {
+  header: {
+    fg: '#ff598a',
+
+  },
+  input: {
+    color: '#fff',
+    background: '#070222',
+    textAligh: 'center',
+  },
+  inputFocus: {
+    outline: '2px solid #5e9fff'
+  }
+}
 
 function App() {
   return (
     <div className="app__newsletter">
-      <Newsletter />
+      <ThemeProvider theme={theme}>
+        <Newsletter />
+      </ThemeProvider>
     </div>
   )
 }
--- 01-inline-styles/before/src/ThemeContext.js
+++ 01-inline-styles/before/src/ThemeContext.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
 export const ThemeContext = React.createContext({ theme: {} })
-
+console.log(ThemeContext)
 export function ThemeProvider(props) {
+  console.log(props.children)
   return (
     <ThemeContext.Provider value={{ theme: props.theme || {} }}>
       {props.children}
